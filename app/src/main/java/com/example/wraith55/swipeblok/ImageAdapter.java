c6d296b945cd61ba0af2f37ec2f2686e7189d2e6
Ronald Shaw <wraith55@gmail.com>
2015-05-08 10:06:05 -0600

    Added comments and cleaned up code

--- app/src/main/java/com/example/wraith55/swipeblok/ImageAdapter.java
+++ app/src/main/java/com/example/wraith55/swipeblok/ImageAdapter.java
@@ -28,23 +28,36 @@ public class ImageAdapter extends BaseAdapter {
         return 0;
     }
 
-    // create a new ImageView for each item referenced by the Adapter
+    /**
+     * Returns the view of the image adapter
+     * @param position in the array
+     * @param convertView if you want a specific review converted to this context
+     * @param parent
+     * @return a view
+     */
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
         if (convertView == null) {
             imageView = new ImageView(mContext);
+            //size of the grid
             imageView.setLayoutParams(new GridView.LayoutParams(175, 175));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            //padding between cells
             imageView.setPadding(8, 8, 8, 8);
         } else {
             imageView = (ImageView) convertView;
         }
-
+        //get the thumbnail from the position
         imageView.setImageResource(mThumbIds[position]);
         return imageView;
     }
 
+    /**
+     * updates the grid view with an array
+     * @param outputArr the array from the main activity which tells the gridview which pictures to load
+     */
     public void update(String[] outputArr){
+        //set all pics to correct according to array
         for(int i=0;i<outputArr.length;i++){
             if(outputArr[i]==MainActivity.empty){
                 mThumbIds[i] = picArray[0];
@@ -68,6 +81,7 @@ public class ImageAdapter extends BaseAdapter {
 
     };
 
+    //the drawables that can be in an array
     public Integer[] picArray = {
             R.drawable.emptyicon, R.drawable.staricon,
             R.drawable.circleicon, R.drawable.cloudicon,
